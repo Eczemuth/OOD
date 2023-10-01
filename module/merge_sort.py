965d66338f4a24b4ded0dabafa41d32b59c86699
best <kittibest46@gmail.com>
2023-10-01 21:58:36 +0700

    <update>: update merge sort to be able to reverse

--- module/merge_sort.py
+++ module/merge_sort.py
@@ -1,21 +1,27 @@
 import random
 
 
-def merge_sort(arr):
+def merge_sort(arr, reverse=False):
     if len(arr) < 2:
         return arr
 
-    half1_len = len(arr)//2
+    half1_len = len(arr) // 2
     local_res = []
-    left_arr = merge_sort(arr[:half1_len])
-    right_arr = merge_sort(arr[half1_len:])
+    left_arr = merge_sort(arr[:half1_len], reverse)
+    right_arr = merge_sort(arr[half1_len:], reverse)
+
     while left_arr and right_arr:
         n1, n2 = left_arr[0], right_arr[0]
-        if n1 < n2:
-            local_res.append(left_arr.pop(0))
+        if reverse:
+            if n1 > n2:
+                local_res.append(left_arr.pop(0))
+            else:
+                local_res.append(right_arr.pop(0))
         else:
-            local_res.append(right_arr.pop(0))
-
+            if n1 < n2:
+                local_res.append(left_arr.pop(0))
+            else:
+                local_res.append(right_arr.pop(0))
     if left_arr:
         local_res.extend(left_arr)
     if right_arr:
@@ -26,5 +32,6 @@ def merge_sort(arr):
 testcases = [[random.randint(-50, 50) for _ in range(random.randint(5, 15))] for _ in range(20)]
 
 for nums_arr in testcases:
-    print(merge_sort(nums_arr) == sorted(nums_arr))
+    res = merge_sort(nums_arr) == sorted(nums_arr)
+    print(nums_arr) if not res else None
 
